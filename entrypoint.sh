6590af81e90d7e98f8694c5f9a08ce74fa28b268
Teszáry Péter <peterteszary@gmail.com>
2025-09-26 11:35:02 +0200

    entripoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# Várakozás, hogy az adatbázis szolgáltatás (ha lenne) elinduljon (jó gyakorlat)
-# Ebben az esetben nem szükséges, de a jövőre nézve hasznos
-# sleep 5
-
 # Lefuttatjuk az adatbázis inicializáló szkriptet
 echo ">>> Running Database Initializer..."
 python init_db.py
 
+# A KULCSFONTOSSÁGÚ ÚJ SOR:
+# Átadjuk az 'instance' mappa (és a benne lévő database.db fájl)
+# tulajdonjogát a 'www-data' felhasználónak és csoportnak.
+# A Gunicorn alapértelmezetten ezt a felhasználót használja.
+echo ">>> Setting ownership of database file..."
+chown -R www-data:www-data /app/instance
+
 # Elindítjuk a fő alkalmazást (a webszervert)
-# Az 'exec' parancs fontos, mert átadja a fő processz szerepét a gunicornnak
 echo ">>> Starting Gunicorn Web Server..."
 exec gunicorn --bind 0.0.0.0:5000 app:app
