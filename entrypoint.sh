bf917e85e9e1e1273ca50886b132f1c48dfbc497
Teszáry Péter <peterteszary@gmail.com>
2025-09-26 11:58:15 +0200

    endpoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,10 @@ echo ">>> Running Database Initializer..."
 python init_db.py
 
 # A KULCSFONTOSSÁGÚ ÚJ SOR:
-# Átadjuk az 'instance' mappa (és a benne lévő database.db fájl)
-# tulajdonjogát a 'www-data' felhasználónak és csoportnak.
-# A Gunicorn alapértelmezetten ezt a felhasználót használja.
-echo ">>> Setting ownership of database file..."
-chown -R www-data:www-data /app/instance
+# Jogosultságot adunk MINDEN felhasználónak, hogy írhassa és olvashassa
+# az 'instance' mappát és a benne lévő fájlokat.
+echo ">>> Setting universal permissions on database directory..."
+chmod -R 777 /app/instance
 
 # Elindítjuk a fő alkalmazást (a webszervert)
 echo ">>> Starting Gunicorn Web Server..."
